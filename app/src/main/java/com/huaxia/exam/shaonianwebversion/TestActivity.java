6e4e1064591f67e24f2f7bb7c88195388c94935a
Hk952700 <1622309994@qq.com>
2019-06-05 15:48:43 +0800

    fenzhi1提交了

--- app/src/main/java/com/huaxia/exam/shaonianwebversion/TestActivity.java
+++ app/src/main/java/com/huaxia/exam/shaonianwebversion/TestActivity.java
@@ -18,7 +18,7 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class TestActivity extends Activity {
-
+    //fenzhi1 2019年6月5日 15:47:33
     private WebView mWebView;
     private ProgressBar mWebProgres;
 
